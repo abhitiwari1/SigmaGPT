2a464d7209ea9f7b430858f108bb88b40dcaded4
abhitiwari1 <151872523+abhitiwari1@users.noreply.github.com>
2025-09-30 10:25:50 +0530

    routes

--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -1,89 +1,89 @@
-// import Router from 'express';
-// import Thread from '../model/Thread.js';
-// import getDeepseekAIAPIResponse from '../utils/openai.js';
+import Router from 'express';
+import Thread from '../model/Thread.js';
+import getDeepseekAIAPIResponse from '../utils/openai.js';
 
-// const router = Router();
+const router = Router();
 
-// router.post("/test", async(req, res) => {
-//     try {
-//         const thread = new Thread({
-//             threadId: "xyz",
-//             title: "Testing New Thread"
-//         });
+router.post("/test", async(req, res) => {
+    try {
+        const thread = new Thread({
+            threadId: "xyz",
+            title: "Testing New Thread"
+        });
 
-//         const response = await thread.save();
-//         res.send(response);
+        const response = await thread.save();
+        res.send(response);
 
-//     } catch (error) {
-//         return res.status(500).json({ error: "Something went wrong" });
-//     }
-// });
+    } catch (error) {
+        return res.status(500).json({ error: "Something went wrong" });
+    }
+});
 
-// router.get("threads", async(req, res) => {
-//     try {
-//         const threads = await Thread.find({}).sort({updatedAt: -1});
-//         res.json(threads);
+router.get("threads", async(req, res) => {
+    try {
+        const threads = await Thread.find({}).sort({updatedAt: -1});
+        res.json(threads);
 
-//     } catch (error) {
-//         return res.status(500).json({ error: "Failed to fetch thread" });
-//     }
-// });
+    } catch (error) {
+        return res.status(500).json({ error: "Failed to fetch thread" });
+    }
+});
 
-// router.get("/thread/:threadId", async(req, res) => {
-//     const {threadId} = req.params;
-//     try {
-//         const thread = await Thread.findOne({threadId});
-//         if(!thread){
-//             return res.status(404).json({message: "Thread not found"});
-//         }
-//         res.send(thread.messages);
-//     } catch (error) {
-//         return res.status(500).json({ error: `Failed to fetch thread of threadId ${threadId}` });
-//     }
-// });
+router.get("/thread/:threadId", async(req, res) => {
+    const {threadId} = req.params;
+    try {
+        const thread = await Thread.findOne({threadId});
+        if(!thread){
+            return res.status(404).json({message: "Thread not found"});
+        }
+        res.send(thread.messages);
+    } catch (error) {
+        return res.status(500).json({ error: `Failed to fetch thread of threadId ${threadId}` });
+    }
+});
 
-// router.delete("/thread/:threadId", async(req, res) => {
-//     const {threadId} = req.params;
-//     try {
-//         const deletedThread = await Thread.findOneAndDelete({threadId});
-//         if(!deletedThread){
-//             return res.status(404).json({message: "Thread not found"});
-//         }
-//         return res.status(200).json({message: "Thread deleted successfully"});
+router.delete("/thread/:threadId", async(req, res) => {
+    const {threadId} = req.params;
+    try {
+        const deletedThread = await Thread.findOneAndDelete({threadId});
+        if(!deletedThread){
+            return res.status(404).json({message: "Thread not found"});
+        }
+        return res.status(200).json({message: "Thread deleted successfully"});
 
-//     } catch (error) {
-//         return res.status(500).send({ error: "Error in deleting thread" });
-//     }
-// });
+    } catch (error) {
+        return res.status(500).send({ error: "Error in deleting thread" });
+    }
+});
 
-// router.post("/chat", async(req, res) => {
-//     const {threadId, message} = req.body;
+router.post("/chat", async(req, res) => {
+    const {threadId, message} = req.body;
 
-//     if(!threadId || !message){
-//         return res.status(400).json({message: "missing required fields"});
-//     }
+    if(!threadId || !message){
+        return res.status(400).json({message: "missing required fields"});
+    }
 
-//     try {
-//         let thread = await Thread.findOne({threadId});
-//         if(!thread){
-//             thread = new Thread({
-//                 threadId,
-//                 title: message,
-//                 messages: [{role: "user", content: message}]
-//             });
-//         }else{
-//             thread.messages.push({role: "user", content: message});
-//         }
-//         const assistantReply = await getDeepseekAIAPIResponse(message);
-//         thread.messages.push({role: "assistant", content: assistantReply});
-//         thread.updatedAt = Date.now;
-//         await thread.save();
-//         res.json({reply: assistantReply});
+    try {
+        let thread = await Thread.findOne({threadId});
+        if(!thread){
+            thread = new Thread({
+                threadId,
+                title: message,
+                messages: [{role: "user", content: message}]
+            });
+        }else{
+            thread.messages.push({role: "user", content: message});
+        }
+        const assistantReply = await getDeepseekAIAPIResponse(message);
+        thread.messages.push({role: "assistant", content: assistantReply});
+        thread.updatedAt = Date.now;
+        await thread.save();
+        res.json({reply: assistantReply});
 
-//     } catch (error) {
-//         return res.status(500).send({ error: "Something went wrong" });
-//     }
-// })
+    } catch (error) {
+        return res.status(500).send({ error: "Something went wrong" });
+    }
+})
 
 
-// export default router;
+export default router;
