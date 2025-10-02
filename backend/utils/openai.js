368ab183c5936e9bd03e420350a64ddbb269bc23
abhitiwari1 <151872523+abhitiwari1@users.noreply.github.com>
2025-10-02 21:29:05 +0530

    utils

--- backend/utils/openai.js
+++ backend/utils/openai.js
@@ -1,32 +1,32 @@
-// import "dotenv/config"
+import "dotenv/config"
 
-// const getDeepseekAIAPIResponse = async (message) => {
-//     const options = {
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "application/json",
-//             "Accept": "application/json",
-//             "Authorization": `Bearer ${process.env.DEEPSEEK_API_KEY}`
-//         },
-//         body: JSON.stringify({
-//             model: "deepseek-chat",
-//             messages: [
-//                 {
-//                     role: "user",
-//                     content: message
-//                 }
-//             ]
-//         })
-//     };
+const getDeepseekAIAPIResponse = async (message) => {
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": `Bearer ${process.env.DEEPSEEK_API_KEY}`
+        },
+        body: JSON.stringify({
+            model: "deepseek-chat",
+            messages: [
+                {
+                    role: "user",
+                    content: message
+                }
+            ]
+        })
+    };
 
-//     try {
-//         const response = await fetch("https://api.deepseek.com/chat/completions", options);
+    try {
+        const response = await fetch("https://api.deepseek.com/chat/completions", options);
 
-//         const data = await response.json();
-//         return data.choices[0].message.content;
-//     } catch (error) {
-//         return res.status(500).json({ error: "Something went wrong" });
-//     }
-// }
+        const data = await response.json();
+        return data.choices[0].message.content;
+    } catch (error) {
+        return res.status(500).json({ error: "Something went wrong" });
+    }
+}
 
-// export default getDeepseekAIAPIResponse;
+export default getDeepseekAIAPIResponse;
