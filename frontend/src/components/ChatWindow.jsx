5bce9b5a76e55da12d2ea9a98c23f88803cf615d
abhitiwari1 <151872523+abhitiwari1@users.noreply.github.com>
2025-10-09 23:39:14 +0530

    frontend

--- frontend/src/components/ChatWindow.jsx
+++ frontend/src/components/ChatWindow.jsx
@@ -1,96 +1,96 @@
-// import './ChatWindow.css'
-// import Chat from './Chat.jsx'
-// import { MyContext } from './MyContext.jsx'
-// import { useContext, useEffect, useState } from 'react'
-// import {ScaleLoader} from 'react-spinners'
+import './ChatWindow.css'
+import Chat from './Chat.jsx'
+import { MyContext } from './MyContext.jsx'
+import { useContext, useEffect, useState } from 'react'
+import {ScaleLoader} from 'react-spinners'
 
-// export default function ChatWindow() {
+export default function ChatWindow() {
 
-//   const {prompt, setPrompt, reply, setReply, currThreadId, prevChats, setPrevChats, setNewChat} = useContext(MyContext);
-//   const [isLoading, setIsLoading] = useState(false);
-//   const [isOpen, setIsOpen] = useState(false);
-
-
-//   const getReply = async() => {
-//     setIsLoading(true);
-//     setNewChat(false);
-//     const options = {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "Application/json"
-//       },
-//       body: JSON.stringify({
-//         message: prompt,
-//         threadId:currThreadId
-//       })
-//     };
-
-//     try {
-//       const response = await fetch("http://localhost:6000/api/chat", options);
-//       const res = await response.json();
-//       setReply(res.reply);
-//     } catch (error) {
-//       console.log(error)
-//     }
-//     setIsLoading(false);
-//   }
-
-//   useEffect(() => {
-//     if(prompt && reply){
-//       setPrevChats(prevChats => {
-//         [...prevChats, {
-//           role: "user",
-//           content: prompt
-//         }, {
-//           role: "assistant",
-//           content: reply
-//         }]
-//       });
-//     }
-//     setPrompt("");
-//   }, [reply]);
-
-//   const handleProfileClick = () => {
-//     setIsOpen(!isOpen);
-//   }
+  const {prompt, setPrompt, reply, setReply, currThreadId, prevChats, setPrevChats, setNewChat} = useContext(MyContext);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
 
-//   return (
-//     <div className='chatWindow'>
-//       <div className="navbar">
-//         <span>SigmaGPT <i className="fa-solid fa-chevron-down"></i></span>
-//         <div className="userIconDiv" onClick={handleProfileClick}>
-//           <span className='userIcon'><i className="fa-solid fa-user"></i></span>
-//         </div>
-//       </div>
-//       {
-//         isOpen && 
-//         <div className="dropDown">
-//           <div className="dropDownItems"><i class="fa-solid fa-gear"></i>Settings</div>
-//           <div className="dropDownItems"><i class="fa-solid fa-cloud-arrow-up"></i>Upgrade Plan</div>
-//           <div className="dropDownItems"><i class="fa-solid fa-arrow-right-from-bracket"></i>Log out</div>
-//         </div>
-//       }
+  const getReply = async() => {
+    setIsLoading(true);
+    setNewChat(false);
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "Application/json"
+      },
+      body: JSON.stringify({
+        message: prompt,
+        threadId:currThreadId
+      })
+    };
 
-//       <Chat/>
+    try {
+      const response = await fetch("http://localhost:6000/api/chat", options);
+      const res = await response.json();
+      setReply(res.reply);
+    } catch (error) {
+      console.log(error)
+    }
+    setIsLoading(false);
+  }
 
-//       <ScaleLoader color='#fff' loading={isLoading}></ScaleLoader>
-//       <div className="chatInput">
-//         <div className="inputBox">
-//           <input placeholder='Ask Anything' 
-//             value={prompt}
-//             onChange={(e) => setPrompt(e.target.value)}
-//             onKeyDown={(e) => e.key === 'Enter' ? getReply(): ''}
-//           />
+  useEffect(() => {
+    if(prompt && reply){
+      setPrevChats(prevChats => {
+        [...prevChats, {
+          role: "user",
+          content: prompt
+        }, {
+          role: "assistant",
+          content: reply
+        }]
+      });
+    }
+    setPrompt("");
+  }, [reply]);
 
-//           <div id='submit' onClick={() => {
-//             getReply();
-//           }}><i className="fa-solid fa-paper-plane"></i></div>
-//         </div>
-//         <p className='info'>
-//           SigmaGPT can make mistakes. Check important info. See Cookie Preferences.
-//         </p>
-//       </div>
-//     </div>
-//   )
-// }
+  const handleProfileClick = () => {
+    setIsOpen(!isOpen);
+  }
+
+
+  return (
+    <div className='chatWindow'>
+      <div className="navbar">
+        <span>SigmaGPT <i className="fa-solid fa-chevron-down"></i></span>
+        <div className="userIconDiv" onClick={handleProfileClick}>
+          <span className='userIcon'><i className="fa-solid fa-user"></i></span>
+        </div>
+      </div>
+      {
+        isOpen && 
+        <div className="dropDown">
+          <div className="dropDownItems"><i class="fa-solid fa-gear"></i>Settings</div>
+          <div className="dropDownItems"><i class="fa-solid fa-cloud-arrow-up"></i>Upgrade Plan</div>
+          <div className="dropDownItems"><i class="fa-solid fa-arrow-right-from-bracket"></i>Log out</div>
+        </div>
+      }
+
+      <Chat/>
+
+      <ScaleLoader color='#fff' loading={isLoading}></ScaleLoader>
+      <div className="chatInput">
+        <div className="inputBox">
+          <input placeholder='Ask Anything' 
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+            onKeyDown={(e) => e.key === 'Enter' ? getReply(): ''}
+          />
+
+          <div id='submit' onClick={() => {
+            getReply();
+          }}><i className="fa-solid fa-paper-plane"></i></div>
+        </div>
+        <p className='info'>
+          SigmaGPT can make mistakes. Check important info. See Cookie Preferences.
+        </p>
+      </div>
+    </div>
+  )
+}
